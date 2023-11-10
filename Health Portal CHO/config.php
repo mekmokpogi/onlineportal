cfca31f5f1ab8f8149cdec0785670f42455fa5e4
Healthportal <145546911+mekmokpogi@users.noreply.github.com>
2023-11-10 19:05:12 +0800

    Delete Health Portal CHO directory

--- Health Portal CHO/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-//used to establish connection to the database
-$db_name = "mysql:host=localhost;dbname=cho_db";
-$username = "root";
-$password = "";
-
-$conn = new PDO($db_name, $username, $password);
-
-?>
